6c729e717a84212defd68c0a7c62df03b4b043d8
unknown <abdallah.sarrawi750@gmail.com>
2023-07-08 13:28:12 +0300

    Final-Setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { ThemeProvider } from '@emotion/react';
 import { theme } from './Theme/index';
 import { Typography } from '@mui/material';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<div>home page</div>} />
+          <Route path="/" element={<HomePage />} />
           <Route path="*" element={<div>not found</div>} />
         </Routes>
       </BrowserRouter>
