e9f39445fbb2abf56135381215e415a899cb7f5e
unknown <abdallah.sarrawi750@gmail.com>
2023-07-08 14:38:29 +0300

    Resolved Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './Theme/index';
-import { Typography } from '@mui/material';
+
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
+import NotFounPage from './pages/NotFoundPage/NotFounPage';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="*" element={<div>not found</div>} />
+          <Route path="*" element={<NotFounPage />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
