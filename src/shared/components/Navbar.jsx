07da37f91d31b0adbdc29ffd9849b7df8c98b7a0
Hakim <abdalhakem1226@gmail.com>
2023-07-09 16:20:07 +0300

    breaking down the header to multiple components relates #7

--- src/shared/components/Navbar.jsx
+++ src/shared/components/Navbar.jsx
@@ -1,95 +1,27 @@
-/* eslint-disable react/no-unescaped-entities */
 import { useState } from 'react';
-import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
+import styled from '@emotion/styled';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import MoreIcon from '@mui/icons-material/MoreVert';
-import { Button } from '@mui/material';
+import SearchBar from './SearchBar';
+import NavMenu from './NavMenu';
+import UserMenu from './UserMenu';
+import MobileUserMenu from './MobileUserMenu';
 
-import {
-  Menu as MenuIcon,
-  Search as SearchIcon,
-  FavoriteBorder,
-  PermIdentity,
-  WorkOutline
-} from '@mui/icons-material';
-
-const pages = ['Handbags', 'Watches', 'Skincare', 'Jewellery', 'Apparels'];
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25)
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto'
-  }
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  color: 'black',
-  zIndex: '1'
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'black',
-  backgroundColor: '#f1f1f1',
-  borderRadius: '2px',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch'
-    }
-  }
+export const Logo = styled(Typography)(({ theme }) => ({
+  marginRight: '2rem',
+  fontFamily: theme.typography.h1.fontFamily,
+  fontWeight: theme.typography.h1.fontWeight,
+  letterSpacing: '.05rem',
+  color: theme.typography.h1.color,
+  textDecoration: 'none'
 }));
 
 function Navbar() {
   const [anchorEl, setAnchorEl] = useState(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
   const [anchorElNav, setAnchorElNav] = useState(null);
-
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-
-  const handleMobileMenuOpen = (event) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -97,72 +29,24 @@ function Navbar() {
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
+    handleMobileMenuClose();
   };
 
-  const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right'
-      }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right'
-      }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}>
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  );
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
-  const renderMobileMenu = (
-    <Menu
-      sx={{ padding: '0' }}
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right'
-      }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right'
-      }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}>
-      <MenuItem>
-        <IconButton size="medium" aria-label="show 4 new mails" color="inherit">
-          <FavoriteBorder />
-        </IconButton>
-        <p>Favorite</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton size="medium" aria-label="show 17 new notifications" color="inherit">
-          <PermIdentity />
-        </IconButton>
-        <p>Lorem</p>
-      </MenuItem>
-      {/* <MenuItem onClick={handleProfileMenuOpen}> */}
-      <MenuItem>
-        <IconButton
-          size="medium"
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit">
-          <WorkOutline />
-        </IconButton>
-        <p>Lorem</p>
-      </MenuItem>
-    </Menu>
-  );
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -170,129 +54,37 @@ function Navbar() {
         position="static"
         sx={{ bgcolor: 'var(--bg-white)', color: 'black', boxShadow: 'none' }}>
         <Toolbar>
-          <Typography
+          <Logo
             variant="h6"
             sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.05rem',
-              color: 'var(--primary-color)',
-              textDecoration: 'none'
+              display: { xs: 'none', md: 'flex' }
             }}>
             CORA'L
-          </Typography>
+          </Logo>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{
-                  my: 2,
-                  color: 'black',
-                  display: 'block',
-                  textTransform: 'capitalize',
-                  fontSize: '.9rem',
-                  paddingInline: '5px'
-                }}>
-                {page}
-              </Button>
-            ))}
-          </Box>
+          <NavMenu
+            handleOpenNavMenu={handleOpenNavMenu}
+            handleCloseNavMenu={handleCloseNavMenu}
+            anchorElNav={anchorElNav}
+          />
 
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: { xs: 'flex', md: 'none' },
-              alignItems: 'center'
-            }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit">
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left'
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left'
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' }
-              }}>
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-            <Typography
-              variant="h6"
-              sx={{
-                ml: 2,
-                display: { xs: 'flex', md: 'none' },
-                fontFamily: 'monospace',
-                fontWeight: 700,
-                letterSpacing: '.05rem',
-                color: 'var(--primary-color)',
-                textDecoration: 'none'
-              }}>
-              CORA'L
-            </Typography>
-          </Box>
-
-          {/* The next box is for spearating the tow sections */}
           <Box sx={{ flexGrow: 1 }} />
-          {/* The next box is for spearating the tow sections */}
 
-          <Search sx={{ display: { xs: 'none', sm: 'inline-block' } }}>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} />
-          </Search>
+          <SearchBar />
 
-          <Box sx={{ display: { xs: 'none', sm: 'flex' } }}>
-            <IconButton size="medium" color="inherit">
-              <FavoriteBorder />
-            </IconButton>
-            <IconButton size="medium" onClick={handleProfileMenuOpen} color="inherit">
-              <PermIdentity />
-            </IconButton>
-            <IconButton edge="end" size="medium" color="inherit">
-              <WorkOutline />
-            </IconButton>
-          </Box>
+          <UserMenu
+            handleProfileMenuOpen={handleProfileMenuOpen}
+            handleMenuClose={handleMenuClose}
+            anchorEl={anchorEl}
+          />
 
-          <Box sx={{ display: { xs: 'flex', sm: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit">
-              <MoreIcon />
-            </IconButton>
-          </Box>
+          <MobileUserMenu
+            handleMobileMenuOpen={handleMobileMenuOpen}
+            handleMobileMenuClose={handleMobileMenuClose}
+            mobileMoreAnchorEl={mobileMoreAnchorEl}
+          />
         </Toolbar>
       </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
     </Box>
   );
 }
