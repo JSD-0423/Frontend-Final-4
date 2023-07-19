c7377f437c60075b72dc99b3b0885a40bd0393dd
Masaabushamma99 <Masaabushamma402@gmail.com>
2023-07-19 11:52:25 +0300

    fix deploy error

--- src/shared/components/IconCard/style.jsx
+++ src/shared/components/IconCard/style.jsx
@@ -1,12 +1,12 @@
 import styled from '@emotion/styled';
 
-export const IconCardContainer = styled.div`
-  background-color: ${(props) => props.backgroundColor};
-  border-radius: 10px;
-  min-width: 168px;
-  min-height: 168px;
-  width: 100%;
-`;
+export const IconCardContainer = styled('div')(({ theme }) => ({
+  backgroundColor: 'cyan',
+  borderradius: '10px',
+  minWidth: '168px',
+  minHeight: '168px',
+  width: '100%'
+}));
 export const IConCardImageContainer = styled('div')({
   display: 'flex',
   justifyContent: 'center',
