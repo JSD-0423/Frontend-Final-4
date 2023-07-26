91b4215c87f56719b097235b659aafb0dd630ffc
Masaabushamma99 <Masaabushamma402@gmail.com>
2023-07-26 19:16:17 +0300

    edit

--- src/shared/components/ImageWithTextBanner/index.jsx
+++ src/shared/components/ImageWithTextBanner/index.jsx
@@ -2,13 +2,7 @@ import { CollectionImg, ImageWithTextBannerStyle, TextOnCollectionImg, TextParag
 
 function ImageWithTextBanner({ imgSrc, headerText, Description }) {
   return (
-    <ImageWithTextBannerStyle>
       <CollectionImg src={imgSrc}/>
-      <TextOnCollectionImg>
-        <h1>{headerText}</h1>
-        <TextParagraph>{Description}</TextParagraph>
-      </TextOnCollectionImg>
-    </ImageWithTextBannerStyle>
   );
 }
 
--- src/shared/components/ImageWithTextBanner/style.jsx
+++ src/shared/components/ImageWithTextBanner/style.jsx
@@ -1,20 +1,6 @@
 import styled from "@emotion/styled";
 
-export const ImageWithTextBannerStyle = styled('div')(({ theme }) => ({
-    position: 'relative'
-  }));
   export const CollectionImg = styled('img')(({ theme }) => ({
     width: '100%',
      paddingInline: '2%'
   }));
-  export const TextOnCollectionImg = styled('div')(({ theme }) => ({
-   position: 'absolute', 
-   top: '27%', 
-   right: '7%',
-   fontSize:'2.7rem' 
-  }));
-  export const TextParagraph = styled('div')(({ theme }) => ({
-    fontSize:'5rem',
-    paddingTop:'20px'
-    
-   }));
