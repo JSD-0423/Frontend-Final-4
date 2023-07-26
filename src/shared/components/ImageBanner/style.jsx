23d8d1db215b68f144e44cce0149c1294f518457
Masaabushamma99 <Masaabushamma402@gmail.com>
2023-07-26 21:35:34 +0300

    set height and object fit

--- src/shared/components/ImageBanner/style.jsx
+++ src/shared/components/ImageBanner/style.jsx
@@ -2,5 +2,7 @@ import styled from "@emotion/styled";
 
   export const CollectionImg = styled('img')(({ theme }) => ({
     width: '100%',
-     paddingInline: '2%'
+     paddingInline: '2%',
+     height:'400px',
+     objectFit:'cover'
   }));
