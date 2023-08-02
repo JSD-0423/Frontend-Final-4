d595c18fae857560b0b5ac96f96e5d4e692122b1
unknown <abdallah.sarrawi750@gmail.com>
2023-08-03 01:02:01 +0300

    new-arrivals-integration

--- src/shared/components/Carousel/CarouselStyle.jsx
+++ src/shared/components/Carousel/CarouselStyle.jsx
@@ -1,12 +1,9 @@
-import { Box, ImageList } from '@mui/material';
+import { ImageList } from '@mui/material';
 import React from 'react';
 import { styled } from '@mui/system';
 
 export const CarouselBox = styled(ImageList)(({ theme }) => ({
-  // overflow: 'auto',
   height: '400px',
   margin: '0px  20px  0px 20px ',
   overflowX: 'auto'
-
-  //   width: '100%'
 }));
